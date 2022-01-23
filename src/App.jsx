fe11c9b91e714a594ee48f0053ff3f8aff4545c4
jiyeonLeeLuda <dlwldusq.work@gmail.com>
2022-01-23 21:38:29 +0900

    home 스케치 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Home from './component/home/Home';
 import NavBar from './component/navbar/NavBar';
 
 const App = (props) => (
   <>
     <NavBar />
+    <Home />
   </>
 );
 
---
+++ src/component/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './home.module.css';
+
+const Home = (props) => {
+  return (
+    <section className={styles.home}>
+      {/* <img className={styles.avatar} src='img/avatar2.jpg' alt='avatar' /> */}
+      <div className={styles.textbox}>
+        <span>Front-End</span>
+        <span>Developer</span>
+        <br />
+        <span>소통하는 개발자</span>
+        <span>이지연 입니다.</span>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
